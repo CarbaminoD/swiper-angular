e6906e4b8530b0f6a6c0f536b61c64413f381dae
PacharaC <31031325+CarbaminoD@users.noreply.github.com>
2023-07-15 20:40:34 +0700

    init component, page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/page/home/home.component';
+import { ContactComponent } from './main/page/contact/contact.component';
+import { NewProductComponent } from './main/section/new-product/new-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
