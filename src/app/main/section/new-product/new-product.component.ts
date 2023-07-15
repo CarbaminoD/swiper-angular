6b6c1f9a6fc7d68ce8c0a34ca168ed61ca4e0f0e
PacharaC <31031325+CarbaminoD@users.noreply.github.com>
2023-07-15 20:51:35 +0700

    new-product components

--- src/app/main/section/new-product/new-product.component.ts
+++ src/app/main/section/new-product/new-product.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var Swiper: any; // Declare Swiper as a global variable
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
   styleUrls: ['./new-product.component.scss']
 })
-export class NewProductComponent {
+export class NewProductComponent implements AfterViewInit  {
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
+  @ViewChild('swiperContainer2') swiperContainer2!: ElementRef;
 
+  ngAfterViewInit(): void {
+    const swiper1 = new Swiper(this.swiperContainer.nativeElement, {
+      // Swiper configuration options
+      spaceBetween: 30,
+      slidesPerView: 'auto',
+      loop: false,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        hide: false,
+      }
+    });
+    
+    const swiper2 = new Swiper(this.swiperContainer2.nativeElement, {
+      // Swiper configuration options
+      spaceBetween: 30,
+      slidesPerView: 'auto',
+      loop: false,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        hide: false,
+      }
+    });
+  }
 }
